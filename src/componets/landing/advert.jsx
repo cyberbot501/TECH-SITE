01a6ea513fe97271c6c6626b6b7eacf89dc6d49e
cyberbot501 <softwareengineerjoycy@gmail.com>
2024-09-15 16:01:30 +0100

    update

--- src/componets/landing/advert.jsx
+++ src/componets/landing/advert.jsx
@@ -25,12 +25,12 @@ export default function Advert() {
         className="mySwiper border-[1px] border-[#171719] w-[100%] overflow-hidden h-[800px] md:w-[975px] md:h-[539px] rounded-[25px]"
       >
         <SwiperSlide className='bg-[#2C3436] rounded-[25px] overflow-hidden'>
-            <div className='flex flex-col gap-5 md:gap-0 md:flex-row items-center'>
-                <img src={girl} alt=""  className='md:w-[459px] w-[298px] h-[300px] md:h-[522px] md:relative top-10'/>
+            <div className='flex flex-col gap-5 md:gap-0 px-[10px] md:flex-row items-center'>
+                <img src={girl} alt=""  className='md:w-[459px] w-[100%] h-[300px] md:h-[522px] md:relative top-10'/>
                 <div className='flex flex-col gap-3 items-center  md:start '>
                     <img src={game} alt="" className='md:w-[60px] w-[45px] h-[20px] md:h-[45px]'/>
-                    <h2 className='font-sans font-bold md:text-[54px] md:text-start w-[290px] text-center  text-[49px] text-white md:w-[476px]'>Experience the Vast Metaverse</h2>
-                    <p className='font-sans text-[#9B9B9B] text-center md:text-start text-[20px] md:text-[26px] md:w-[476px] w-[290px] font-medium'>Explore the vast metaverse, unravel the marvelous details of the verse through our blaq VR-headset.</p>
+                    <h2 className='font-sans font-bold md:text-[54px] md:text-start  text-center  text-[49px] text-white md:w-[476px]'>Experience the Vast Metaverse</h2>
+                    <p className='font-sans text-[#9B9B9B] text-center md:text-start text-[20px] md:text-[26px] md:w-[476px] font-medium'>Explore the vast metaverse, unravel the marvelous details of the verse through our blaq VR-headset.</p>
                     <div className='flex flex-row gap-5 items-center'>
                         <button className='w-[122px] h-[49px] bg-[#B0EF9A] rounded-[21px] text-[17.5px] font-sans font-medium text-[#171719]'>
                             Buy Now
@@ -44,12 +44,12 @@ export default function Advert() {
             </div>
         </SwiperSlide>
         <SwiperSlide className='bg-[#2C3436] rounded-[25px] overflow-hidden'>
-            <div className='flex flex-col gap-5 md:gap-0 md:flex-row items-center'>
-                <img src={girl} alt=""  className='md:w-[459px] w-[298px] h-[300px] md:h-[522px] md:relative top-10'/>
+            <div className='flex flex-col gap-5 md:gap-0 px-[10px] md:flex-row items-center'>
+                <img src={girl} alt=""  className='md:w-[459px] w-[100%] h-[300px] md:h-[522px] md:relative top-10'/>
                 <div className='flex flex-col gap-3 items-center  md:start '>
                     <img src={game} alt="" className='md:w-[60px] w-[45px] h-[20px] md:h-[45px]'/>
-                    <h2 className='font-sans font-bold md:text-[54px] md:text-start w-[290px] text-center  text-[49px] text-white md:w-[476px]'>Experience the Vast Metaverse</h2>
-                    <p className='font-sans text-[#9B9B9B] text-center md:text-start text-[20px] md:text-[26px] md:w-[476px] w-[290px] font-medium'>Explore the vast metaverse, unravel the marvelous details of the verse through our blaq VR-headset.</p>
+                    <h2 className='font-sans font-bold md:text-[54px] md:text-start  text-center  text-[49px] text-white md:w-[476px]'>Experience the Vast Metaverse</h2>
+                    <p className='font-sans text-[#9B9B9B] text-center md:text-start text-[20px] md:text-[26px] md:w-[476px] font-medium'>Explore the vast metaverse, unravel the marvelous details of the verse through our blaq VR-headset.</p>
                     <div className='flex flex-row gap-5 items-center'>
                         <button className='w-[122px] h-[49px] bg-[#B0EF9A] rounded-[21px] text-[17.5px] font-sans font-medium text-[#171719]'>
                             Buy Now
