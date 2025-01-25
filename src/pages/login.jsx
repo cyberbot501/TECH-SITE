7257d00b8ddad3f4b1b72820f8d7ca1f6aa5f299
cyberbot501 <softwareengineerjoycy@gmail.com>
2025-01-25 14:13:40 +0100

    update

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,9 +4,9 @@ import girl from '../assets/girl.svg'
 export default function Login() {
   return (
     <div className='bg-[#171719] flex justify-center place-items-center content-center h-[100vh]'>
-      <div className='w-[700px] h-[400px] rounded-[40px] bg-[#2C3436] shadow-2xl overflow-hidden flex flex-row gap-9 items-center justify-center'>
-        <img src={girl} alt="" className='w-[400px] h-[500px]' />
-        <form action="" className='flex flex-col justify-center items-center gap-5'>
+      <div className='w-[750px] h-[500px] rounded-[40px] bg-[#2C3436] shadow-2xl overflow-hidden flex flex-row gap-5 items-center justify-start'>
+        <img src={girl} alt="" className='w-[400px] h-[650px]' />
+        <form action="" className='flex flex-col justify-center items-center gap-4'>
 
           <h2 className='text-white text-[54px] font-bold'>Login</h2>
 
@@ -20,6 +20,13 @@ export default function Login() {
             <p className='text-white text-[22px] font-semibold'>Password</p>
             <input type="password" name="" id="" className='w-[250px] h-[30px] rounded-[30px] shadow'/>
           </label>
+
+          <div className='flex flex-col justify-center'>
+          <p className='text-[#B0EF9A] text-center'>Forgotten Passoword</p>
+
+<p className='text-[#acacac]'>Don't Have an account? <spam className='text-[#B0EF9A]'>SignUP</spam></p>
+
+          </div>
 
           <button className='w-[122px] h-[44px] bg-[#B0EF9A] rounded-[25px] text-[17.5px] font-sans font-medium text-[#171719] '>
             Login
